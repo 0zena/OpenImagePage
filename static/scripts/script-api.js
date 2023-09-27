08027d84ddcc7a53def072525c9e987ebe241d3d
zena <jevgenijs.kursevs@gmail.com>
2023-09-27 22:02:18 +0300

    2nd commit

--- static/scripts/script-api.js
+++ static/scripts/script-api.js
@@ -1,5 +1,5 @@
-const accessKey  = ``;
-const link = `https://api.unsplash.com/photos/random/?count=30&client_id=${accessKey}`;
+const token  = config.accessKey;
+const link = `https://api.unsplash.com/photos/random/?count=30&client_id=${token}`;
 const divContainer = document.getElementById("image-container");
 
 async function GetRequest() {
@@ -8,7 +8,11 @@ async function GetRequest() {
         const photos = await response.json();
         
         photos.forEach(element => {
-            divContainer.innerHTML += `<div><img src="${element.urls.regular}" alt="API"> <span>${element.alt_description}</span></div>`;
+            divContainer.innerHTML +=
+            `<div class="blur-image">
+                <img src="${element.urls.regular}" loading="lazy"> 
+                <span>${element.alt_description}</span> 
+            </div>`;
         });
     }
     catch (error) {
