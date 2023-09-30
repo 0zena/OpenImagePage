56b8449b8f9f9b801d158c1060c3157a487addee
zena <jevgenijs.kursevs@gmail.com>
2023-09-30 15:03:56 +0300

    search bar added

--- static/scripts/script-api.js
+++ static/scripts/script-api.js
@@ -1,23 +1,51 @@
 const token  = config.accessKey;
-const link = `https://api.unsplash.com/photos/random/?count=30&client_id=${token}`;
+const link1 = `https://api.unsplash.com/photos/random/?count=30&client_id=${token}`;
 const divContainer = document.getElementById("image-container");
 
-async function GetRequest() {
+async function GetRequest(link) {
     try {
         const response = await fetch(link);
         const photos = await response.json();
         
-        photos.forEach(element => {
-            divContainer.innerHTML +=
-            `<div class="blur-image">
-                <img src="${element.urls.regular}" loading="lazy"> 
+        if (Array.isArray(photos)) {
+            divContainer.innerHTML = "";
+            
+            photos.forEach(element => {
+                divContainer.innerHTML +=
+                `<div class="blurred-image">
+                <img src="${element.urls.small}" loading="lazy" id="image-box"> 
                 <span>${element.alt_description}</span> 
-            </div>`;
-        });
+                </div>`;
+            });
+        } else if (photos.results && Array.isArray(photos.results)) {
+            divContainer.innerHTML = "";
+            
+            photos.results.forEach(element => {
+                divContainer.innerHTML +=
+                    `<div class="blurred-image" >
+                    <img src="${element.urls.small}" loading="lazy" id="image-box"> 
+                    <span>${element.alt_description}</span> 
+                    </div>`;
+            });
+        }
     }
     catch (error) {
+        console.error("Unexpected API response:", data);
         alert("Can not load photos");
     }
 }
 
-GetRequest();
+GetRequest(link1);
+
+const searchBar = document.querySelector('#search-bar');
+
+searchBar.addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        if (this.value != "") { 
+            const link2 = `https://api.unsplash.com/search/photos?client_id=${token}&query=${this.value}&per_page=30`;
+            GetRequest(link2);
+        }
+        this.value = ""
+    }
+});
+
--- static/scripts/script-menu.js
+++ static/scripts/script-menu.js
@@ -2,7 +2,6 @@ const MenuTap = document.getElementById("corner-menu-button");
 const CornerMenu = document.getElementById("corner-popup-menu");
 
 function ShowMenu() {
-
     MenuTap.addEventListener("click", (Event) => {
         CornerMenu.style.right = "0px"
         HideMenu();
