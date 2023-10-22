bd9e516dc22706438a8b6e859c26c59f359e4a81
zena <jevgenijs.kursevs@gmail.com>
2023-10-22 21:10:44 +0300

    server.js update, bug fix and changes in code structure

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+//#region const
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path');
+const path = require('path')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:3000')
+//#endregion
 
-app.use('/static', express.static('static'));
+app.use('/static', express.static('static'))
+app.use('/views', express.static('views'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
-  
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/signin.html'))
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
 app.listen(port, () => {
   console.log(`Hosting port ${port}`)
 })
--- static/scripts/script-menu.js
+++ static/scripts/script-menu.js
@@ -6,7 +6,7 @@ menuTap.addEventListener("click", () => {
     if (menu == null) {
         cornerMenu.innerHTML += `
         <aside id="corner-popup-menu">
-            <a href=""><p>About Pontarest</p></a>
+            <a href=""><p>About OpenImg</p></a>
             <a href=""><p>See terms & privacy</p></a>
             <a href=""><p>Contact us</p></a>
         </aside>`;
