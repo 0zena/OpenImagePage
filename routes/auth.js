67ec4c73be8951737615cacd860557187ac97993
zena <jevgenijs.kursevs@gmail.com>
2023-11-05 23:09:54 +0200

    insert into db

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,27 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const router = express.Router()
 
+const path = require('path')
+
+const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: true }));
 
+const mySQL = require('mysql')
+
+const dataBase = mySQL.createConnection({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  })
+
 router.post('/register', (req, res) => {
-    const { name, email, password } = req.body
+    const name = req.body.username
+    const email = req.body.email 
+    const password = req.body.password
     
+    dataBase.query(`INSERT INTO userregister (Username, Email, Password) VALUES ( '${name}', '${email}', '${password}' )`)
+    res.send("sent")
 })
 
 module.exports = router
