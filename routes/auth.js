a5add55affa8e06bb12d95e9127f1376b70a0516
zena <jevgenijs.kursevs@gmail.com>
2023-12-04 20:34:24 +0200

    JWT and Cookie auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const mySQL = require('mysql')
+const cookieParser = require('cookie-parser')
+
 const router = express.Router()
 
-const bcrypt = require('bcrypt')
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(express.json())
+router.use(cookieParser())
+
 const saltRounds = 10
-
-const bodyParser = require('body-parser')
-router.use(bodyParser.urlencoded({ extended: true }));
-
-const mySQL = require('mysql');
 
 const dataBase = mySQL.createConnection({
     host: process.env.DB_HOST, 
@@ -18,50 +22,71 @@ const dataBase = mySQL.createConnection({
 
 router.post('/register', async (req, res) => {
   const salt =  await bcrypt.genSalt(saltRounds)
+  const sqlQ = `SELECT Email FROM users WHERE Email = ?`
 
   const name = req.body.username
   const email = req.body.email 
   const password = await bcrypt.hash(req.body.password, salt)
 
-  const sqlQ = `SELECT Email FROM users WHERE Email = ?`
+
   dataBase.query(sqlQ, [email], (error, results) => {
     if (error) { 
-      console.log('Database error:', error)
-      res.status(500).json({error: 'Database error'})
+      res
+        .status(500)
+        .json({ error: 'Database error' })
     } 
     if (results.length > 0) { 
-      res.status(400).json({error: 'User with this email already exists'})
+      res
+        .status(400)
+        .json({ error: 'User with this email already exists' })
     } else { 
       dataBase.query(`CALL RegisterUser('${name}', '${email}', '${password}')`)
-      res.status(200).json({message: 'User registered'})
+
+      res
+        .status(200)
+        .json({ message: 'Register succesfully'})
     }
   })
 })
 
 router.post('/signin', async (req, res) => {
+  const sqlL = `SELECT Username, Email, Password FROM users WHERE Email = ?`
+
   const login = req.body.login
   const password = req.body.password
-  
-  const sqlL = `SELECT Username, Email, Password FROM users WHERE Email = ?`
 
   dataBase.query(sqlL, [login], async (error, results) => {
     if (error) { 
       console.log('Database error:', error)
-      res.status(500).json({error: 'Database error'}) 
+      res
+      .status(500)
+      .json({ error: 'Database error' }) 
     }
     if (results.length > 0) {
-      const hashedPassword = results[0].Password;
-      const passwordMatch = await bcrypt.compare(password, hashedPassword);
+      const hashedPassword = results[0].Password
+      const passwordMatch = await bcrypt.compare(password, hashedPassword)
 
       if (passwordMatch) {
-        res.status(200).json({ message: 'Login successful', user: results[0] });
+        const token = jwt.sign({ username: results[0].Username, email: results[0].Email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        res
+          .cookie('user-auth', token, {httpOnly: true})
+          .status(200)
+          .json({ 
+            token: token,
+            message: 'Login successful', 
+            user: results[0].Email 
+          })
       } else {
-        res.status(400).json({ error: 'Failed, email or password are incorrect' });
+        res
+          .status(400)
+          .json({ error: 'Failed, email or password are incorrect' })
       }
     } else {
-      res.status(400).json({ error: 'Failed, email or password are incorrect' });
+      res
+        .status(400)
+        .json({ error: 'Failed, email or password are incorrect' })
     }
-  });
-}) 
+  })
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const port = 3000
 const path = require('path')
 const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 dotenv.config({path: './.env'})
 
@@ -20,13 +22,17 @@ app.use('/auth', require('./routes/auth'))
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/signin.html'))
+app.get('/login', authToken, (req, res) => {
+  if(!authToken) {
+    res.sendFile(path.join(__dirname, '/views/signin.html'))
+  }
+  res.sendFile(path.join(__dirname, '/views/user-profile.html'))
 })
 
 app.get('/register', (req, res) => {
@@ -36,6 +42,33 @@ app.get('/register', (req, res) => {
 app.listen(port, () => {
   console.log(`Hosting port: ${port}`)
 })
+
+function authToken(req, res, next) {
+  const cookies = req.headers.cookie;
+  
+  if (!cookies) {
+    return res.status(401).send('Unauthorized')
+  }
+
+  const cookieArray = cookies.split(';').map(cookie => cookie.trim().split('='));
+  const tokenPair = cookieArray.find(cookie => cookie[0] === 'user-auth');
+
+  if (!tokenPair) {
+    return res.status(401).send('Unauthorized')
+  }
+
+  const token = tokenPair[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.error('JWT Verification Error:', err)
+      return res.status(401).send('Unauthorized')
+    }
+    req.user = user;
+    next()
+  })
+  return true
+}
 
 dataBase.connect( (error) => {
   let msg = error ? "FAILED" : "CONNECTED"
